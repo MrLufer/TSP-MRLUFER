d00aa1b1933645e0add2840c597ef666118f6dfe
Luis Fernando Apaza Gutierrez <lapaza@timejobs.work>
2022-01-12 13:10:50 -0500

    home

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./Home.jsx";
-
-import Tsp from "./Tsp.jsx";
 const App = (props) => (
   <BrowserRouter>
     <div id="margen">

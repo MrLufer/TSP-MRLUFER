356a448c70b120751e186432494b21d133d8f8f4
Luis Fernando Apaza Gutierrez (via Travis CI) <lapaza@timejobs.work>
2022-01-12 18:22:31 +0000

    Deploy release

---
+++ precache-manifest.2fb793adba29678cebe1658e04feea81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4c3ee55a9b72012a340ac705ac1b63",
+    "url": "/TSP-MRLUFER/index.html"
+  },
+  {
+    "revision": "e37e66ec227be220158c",
+    "url": "/TSP-MRLUFER/static/css/main.ff1e6f7c.chunk.css"
+  },
+  {
+    "revision": "65dc0f866de4730e5b78",
+    "url": "/TSP-MRLUFER/static/js/2.87e79ff9.chunk.js"
+  },
+  {
+    "revision": "e37e66ec227be220158c",
+    "url": "/TSP-MRLUFER/static/js/main.f1b40cf4.chunk.js"
+  },
+  {
+    "revision": "d3e2a3ce4a75c68a8f6f",
+    "url": "/TSP-MRLUFER/static/js/runtime~main.cc4166f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.caf1087f243e6b907a5dbdb2c92c1e8e.js"
+  "/TSP-MRLUFER/precache-manifest.2fb793adba29678cebe1658e04feea81.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TSP-MRLUFER/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

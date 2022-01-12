d2f0cada23c8060661bc56ae2c463548b1e5acbc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-01-12 18:12:30 +0000

    Deploy MrLufer/TSP-MRLUFER to github.com/MrLufer/TSP-MRLUFER.git:gh-pages

---
+++ precache-manifest.caf1087f243e6b907a5dbdb2c92c1e8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b5c218599413cce78e6e2cf87ada3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e37e66ec227be220158c",
+    "url": "/static/css/main.ff1e6f7c.chunk.css"
+  },
+  {
+    "revision": "65dc0f866de4730e5b78",
+    "url": "/static/js/2.87e79ff9.chunk.js"
+  },
+  {
+    "revision": "e37e66ec227be220158c",
+    "url": "/static/js/main.f1b40cf4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

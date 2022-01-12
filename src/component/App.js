ea91cd57ed7a651150dbbb640372ea5995bfa9e2
Luis Fernando Apaza Gutierrez <lapaza@timejobs.work>
2022-01-12 13:07:32 -0500

    add home

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home.jsx";
 
 import Tsp from "./Tsp.jsx";
-const App = props => (
+const App = (props) => (
   <BrowserRouter>
     <div id="margen">
-      
-      <Route exact path="/" component={Tsp} />
+      <Route exact path="/" component={Home} />
     </div>
   </BrowserRouter>
 );
